32bd6eca70a61ddc66b5f4d8d2d35875bbd248ef
Erik Ostermueller <eostermueller@gmail.com>
2019-03-03 13:34:27 -0600

    Material paginated cards example with json from http

--- ver.sh
+++ ver.sh
@@ -1,4 +1,17 @@
 echo "npm version:" 
 npm -v
+echo "Node version:"
+node -v
+echo "Node Process"
+node -p process.versions
+echo "Node Platform:"
+node -p process.platform
+echo "Node architecture: "
+node -p process.arch
+echo "node-sass version:"
+node -p "require('node-sass').info"
+echo "npm node-sass versions:"
+npm ls node-sass
+
 ng v
 
